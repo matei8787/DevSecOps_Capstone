72a02f93e22c8608f5f4054dcabcfa9f338f9a90
matei8787 <matei8787@gmail.com>
2025-10-22 14:04:59 +0300

    first commit

---
+++ bugtracker/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$DATABASE" = "postgres" ]; then
+
+    echo "Waiting for connection..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+        sleep 0.1
+    done
+
+    echo "PostgreSQL up and ready!"
+fi
+
+#python manage.py flush --no-input
+#python manage.py migrate
+
+exec "$@"
